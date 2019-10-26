de7c1c5154e61a4ba055615e5a176c2cbe17131f
johnzhouinfo <work@johnzhou.info>
2019-10-26 03:55:02 -0400

    feat: * Implement test for sin & cos functions
    Signed-off-by: johnzhouinfo <work@johnzhou.info>

--- src/TrigFunction.java
+++ src/TrigFunction.java
@@ -1,7 +1,18 @@
-
+/**
+ * @Author Lingjue Zhou
+ * @Date 2019/10/25
+ */
 public class TrigFunction {
 
-    public static String initial(){
+    public static String initial() {
         return "Hello world!";
+    }
+
+    public static Double sin(double x) {
+        return 0.0;
+    }
+
+    public static Double cos(double v) {
+        return 0.0;
     }
 }
--- test/TrigFunctionTest.java
+++ test/TrigFunctionTest.java
@@ -9,4 +9,20 @@ public class TrigFunctionTest {
     public void initial() {
         Assert.assertEquals("Hello world!", TrigFunction.initial());
     }
+
+    @Test
+    public void sinSuccessTest() {
+        Assert.assertEquals(0.0, java.util.Optional.of(TrigFunction.sin(0)));
+        Assert.assertEquals(1.0, java.util.Optional.of(TrigFunction.sin(Math.PI / 2)));
+        Assert.assertEquals(0.0, java.util.Optional.of(TrigFunction.sin(Math.PI)));
+        Assert.assertEquals(-1.0, java.util.Optional.of(TrigFunction.sin((3 * Math.PI) / 2)));
+    }
+
+    @Test
+    public void cosSuccessTest() {
+        Assert.assertEquals(1.0, java.util.Optional.of(TrigFunction.cos(0)));
+        Assert.assertEquals(0.0, java.util.Optional.of(TrigFunction.cos(Math.PI / 2)));
+        Assert.assertEquals(-1.0, java.util.Optional.of(TrigFunction.cos(Math.PI)));
+        Assert.assertEquals(0.0, java.util.Optional.of(TrigFunction.cos((3 * Math.PI) / 2)));
+    }
 }
